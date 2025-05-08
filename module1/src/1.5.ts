632c29e6a1a359fede734a1b2d5c92b3ee901207
zasimmallick <zasimmallickofficial@gmail.com>
2025-05-08 17:54:08 +0600

    first commit

---
+++ module1/src/1.5.ts
@@ -0,0 +1,18 @@
+// {
+//   // Reference Type --> Object
+
+//   const user: {
+//     readonly company: string; // type --> literal types
+//     firstName: string;
+//     middleName?: string; // optional type
+//     lastName: string;
+//     isMarried: boolean;
+//   } = {
+//     company: "Programming Hero",
+//     firstName: "ZASIM",
+//     lastName: "Mallik",
+//     isMarried: false,
+//   };
+
+//   user.company = "PH";
+// }
